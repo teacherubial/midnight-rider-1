0e58af9fd159b0226701369dc1f621c7d4073d21
Tim Ubial <tubial@sd38.bc.ca>
2021-01-07 10:09:29 -0800

    Fast speed implemented

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ REACH THE END BEFORE THE MAN GON GETCHU.
 
 CHOICES = """
     ----
+    C. Speed ahead at full throttle.
     D. Stop to refuel (NO FOOD AVAILABLE)
     E. Status Check
     Q. QUIT
@@ -68,7 +69,26 @@ def main():
 
         user_choice = input("What do you want to do? ").lower().strip("!,.? ")
 
-        if user_choice == "d":
+        if user_choice == "c":
+            # FAST
+            players_distance_now = random.randrange(10, 16)
+            agents_distance_now = random.randrange(7, 15)
+
+            # Burn fuel
+            fuel -= random.randrange(5, 11)
+
+            # Player distance traveled
+            kms_travelled += players_distance_now
+
+            # Agents distance traveled
+            agents_distance -= players_distance_now - agents_distance_now
+
+            # Feedback to Player
+            print()
+            print("ZOOOOOOOM.")
+            print(f"-------- You traveled {players_distance_now} kms.")
+            print()
+        elif user_choice == "d":
             # Refueling
             # Fill up the fuel tank
             fuel = MAX_FUEL_LEVEL
